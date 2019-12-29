8d4f0535c517c616f5b0ae0cf1c0a3ddc6a6d4e3
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-12-29 17:50:27 +0900

    :recycle: (containers) stateをわかりやすく

--- packages/containers/lib/hooks/count/useCount2/useCount2.ts
+++ packages/containers/lib/hooks/count/useCount2/useCount2.ts
@@ -1,10 +1,20 @@
 import { useState, useCallback } from "react";
 
-export const useCount2 = () => {
-  const [value, setValue] = useState(0);
+const initState = {
+  value: 0,
+  increment: 1,
+  decrement: 1
+};
+
+export const useCount2 = (state = initState) => {
+  const [value, setValue] = useState(state.value);
   const set5 = useCallback(() => setValue(5), []);
-  const onIncrement = useCallback(() => setValue(value + 1), [value]);
-  const onDecrement = useCallback(() => setValue(value - 1), [value]);
+  const onIncrement = useCallback(() => setValue(value + state.increment), [
+    value
+  ]);
+  const onDecrement = useCallback(() => setValue(value - state.decrement), [
+    value
+  ]);
 
   return { value, set5, onIncrement, onDecrement };
 };
