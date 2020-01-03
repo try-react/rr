1b96174f6e2f9320def3d889a655355ff975bc49
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-01-03 14:08:06 +0900

    :recycle:

--- packages/containers/lib/hooks/count/useCount2/useCount2.ts
+++ packages/containers/lib/hooks/count/useCount2/useCount2.ts
@@ -1,20 +1,14 @@
 import { useState, useCallback } from "react";
 
 const initState = {
-  value: 0,
-  increment: 1,
-  decrement: 1
+  value: 0
 };
 
 export const useCount2 = (state = initState) => {
   const [value, setValue] = useState(state.value);
   const set5 = useCallback(() => setValue(5), []);
-  const onIncrement = useCallback(() => setValue(value + state.increment), [
-    value
-  ]);
-  const onDecrement = useCallback(() => setValue(value - state.decrement), [
-    value
-  ]);
+  const onIncrement = useCallback(() => setValue(value + 1), [value]);
+  const onDecrement = useCallback(() => setValue(value - 1), [value]);
 
   return { value, set5, onIncrement, onDecrement };
 };
