a350015524718e79dabf78022b00fb8b1ae41347
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-01-19 12:42:33 +0900

    :white_check_mark: (shared) てすとのらいぶらり改善

--- packages/shared/lib/wrapper.tsx
+++ packages/shared/lib/wrapper.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "@app/containers/lib/redux";
-import { Provider as CTXProvider } from "@app/containers/lib/context";
-
-export const wrapper: React.FC = ({ children }) => (
-  <CTXProvider>{children}</CTXProvider>
-);
+import { ContextProvider } from "@app/containers/lib/HOC";
 
 export const reduxWrapper: () => React.FC = () => ({ children }) => (
   <Provider store={store}>{children}</Provider>
 );
+
+export const wrapper = ContextProvider(({ children }) => <>{children}</>);
